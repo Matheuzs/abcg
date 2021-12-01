a886a556c1cc045a09fb9891e0ab7883446ed555
J0tten <henriquejotten@gmail.com>
2021-11-30 22:48:42 -0300

    Criandoterceiro
    Criando o projeto

---
+++ examples/SolarMK2/openglwindow.hpp
@@ -0,0 +1,115 @@
+#ifndef OPENGLWINDOW_HPP_
+#define OPENGLWINDOW_HPP_
+
+#include <string_view>
+
+#include "abcg.hpp"
+#include "model.hpp"
+#include "trackball.hpp"
+
+class OpenGLWindow : public abcg::OpenGLWindow {
+ protected:
+  void handleEvent(SDL_Event& ev) override;
+  void initializeGL() override;
+  void paintGL() override;
+  void paintUI() override;
+  void resizeGL(int width, int height) override;
+  void terminateGL() override;
+
+ private:
+  int m_viewportWidth{};
+  int m_viewportHeight{};
+
+  Model m_model;
+  Model moon_model;
+  int m_trianglesToDraw{};
+  int moon_trianglesToDraw{};
+
+  TrackBall m_trackBallModel;
+  TrackBall m_trackBallLight;
+  float m_zoom{};
+
+  glm::mat4 m_modelMatrix{1.0f};
+  glm::mat4 m_viewMatrix{1.0f};
+  glm::mat4 m_projMatrix{1.0f};
+
+  glm::mat4 moon_modelMatrix{1.0f};
+  glm::mat4 moon_viewMatrix{1.0f};
+  glm::mat4 moon_projMatrix{1.0f};
+
+  // Shaders
+  std::vector<const char*> m_shaderNames{
+      "normalmapping", "texture", "blinnphong", "phong",
+      "gouraud",       "normal",  "depth", "cubereflect"};
+  std::vector<GLuint> m_programs;
+  int m_currentProgramIndex{};
+
+  // View Types
+  std::vector<const char*> m_viewTypesTags{
+      "Basic", "Night Lights", "No Water", "Political", "Disco"};
+  std::vector<GLuint> m_viewTypes;
+  int m_typeIndex{};
+  std::string getViewType(int index);
+
+  // Mapping mode
+  // 0: triplanar; 1: cylindrical; 2: spherical; 3: from mesh
+  int m_mappingMode{};
+
+  // Light and material properties
+  glm::vec4 m_lightDir{-1.0f, -1.0f, -1.0f, 0.0f};
+  glm::vec4 m_Ia{1.0f};
+  glm::vec4 m_Id{1.0f};
+  glm::vec4 m_Is{0.25f};
+  glm::vec4 m_Ka;
+  glm::vec4 m_Kd;
+  glm::vec4 m_Ks;
+  float m_shininess{};
+
+  // Light and material properties
+  glm::vec4 moon_lightDir{-1.0f, -1.0f, -1.0f, 0.0f};
+  glm::vec4 moon_Ia{1.0f};
+  glm::vec4 moon_Id{1.0f};
+  glm::vec4 moon_Is{0.25f};
+  glm::vec4 moon_Ka;
+  glm::vec4 moon_Kd;
+  glm::vec4 moon_Ks;
+  float moon_shininess{};
+
+  // Skybox vertices
+  const std::array<glm::vec3, 36>  m_skyPositions{
+    // Front
+    glm::vec3{-1, -1, +1}, glm::vec3{+1, -1, +1}, glm::vec3{+1, +1, +1},
+    glm::vec3{-1, -1, +1}, glm::vec3{+1, +1, +1}, glm::vec3{-1, +1, +1},
+    // Back
+    glm::vec3{+1, -1, -1}, glm::vec3{-1, -1, -1}, glm::vec3{-1, +1, -1},
+    glm::vec3{+1, -1, -1}, glm::vec3{-1, +1, -1}, glm::vec3{+1, +1, -1},
+    // Right
+    glm::vec3{+1, -1, -1}, glm::vec3{+1, +1, -1}, glm::vec3{+1, +1, +1},
+    glm::vec3{+1, -1, -1}, glm::vec3{+1, +1, +1}, glm::vec3{+1, -1, +1},
+    // Left
+    glm::vec3{-1, -1, +1}, glm::vec3{-1, +1, +1}, glm::vec3{-1, +1, -1},
+    glm::vec3{-1, -1, +1}, glm::vec3{-1, +1, -1}, glm::vec3{-1, -1, -1},
+    // Top
+    glm::vec3{-1, +1, +1}, glm::vec3{+1, +1, +1}, glm::vec3{+1, +1, -1},
+    glm::vec3{-1, +1, +1}, glm::vec3{+1, +1, -1}, glm::vec3{-1, +1, -1},
+    // Bottom
+    glm::vec3{-1, -1, -1}, glm::vec3{+1, -1, -1}, glm::vec3{+1, -1, +1},
+    glm::vec3{-1, -1, -1}, glm::vec3{+1, -1, +1}, glm::vec3{-1, -1, +1}
+  };
+  
+  const std::string m_skyShaderName{"skybox"};
+  GLuint m_skyVAO{};
+  GLuint m_skyVBO{};
+  GLuint m_skyProgram{};
+
+  void initializeSkybox();
+  void renderSkybox();
+  void terminateSkybox();
+  void loadModel(std::string_view path);
+  void loadMoonModel(std::string_view path);
+  void update();
+
+
+};
+
+#endif
